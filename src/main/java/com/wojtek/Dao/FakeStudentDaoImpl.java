b463ac3058b998da4a3f4f85fdad7284d44148c1
WojciechRekosiewicz <wojtek24a@gmail.com>
2019-01-31 21:03:53 +0100

    Initial commit

---
+++ src/main/java/com/wojtek/Dao/FakeStudentDaoImpl.java
@@ -0,0 +1,62 @@
+package com.wojtek.Dao;
+
+import com.wojtek.Entity.Student;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Repository;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+
+@Repository
+@Qualifier("fakeData")
+public class FakeStudentDaoImpl implements StudentDao {
+
+    private static Map<Integer, Student> students;
+
+    static {
+
+        students = new HashMap<Integer, Student>(){
+
+            {
+                put(1, new Student(1, "Susan", "IT"));
+                put(2, new Student(2, "Ahmed", "Bombing"));
+                put(3, new Student(3, "Roh", "Law"));
+                put(4, new Student(4, "Lara", "SToys"));
+            }
+        };
+    }
+
+
+    @Override
+    public Collection<Student> getAllStudents(){
+        return this.students.values();
+    }
+
+
+
+    @Override
+    public Student getStudentById(int id){
+        return this.students.get(id);
+    }
+
+    @Override
+    public void removeStudentById(int id) {
+        this.students.remove(id);
+    }
+
+    @Override
+    public void updateStudent(Student student){
+        Student s = students.get(student.getId());
+        s.setCourse(student.getCourse());
+        s.setName(student.getName());
+        students.put(student.getId(), student);
+    }
+
+    @Override
+    public void insertStudentToDb(Student student) {
+        this.students.put(student.getId(), student);
+    }
+}
---
+++ src/main/java/com/wojtek/Dao/StudentDao.java
@@ -0,0 +1,17 @@
+package com.wojtek.Dao;
+
+import com.wojtek.Entity.Student;
+
+import java.util.Collection;
+
+public interface StudentDao {
+    Collection<Student> getAllStudents();
+
+    Student getStudentById(int id);
+
+    void removeStudentById(int id);
+
+    void updateStudent(Student student);
+
+    void insertStudentToDb(Student student);
+}
